cd76f53f1a1efd294f9bd650128ae6e4194503d6
Roshani5476 <roshdahire1@gmail.com>
2026-01-16 16:36:20 +0530

    Create day03.1_ Table_relationships.sql

---
+++ day03.1_ Table_relationships.sql
@@ -0,0 +1,17 @@
+--Tables are connected using common column called KEYS.
+
+--PRIMARY KEY: 
+--Uniqely identifies each row in a table
+
+--FOREIGN KEY:
+-- A column that refers to the primary key of another table
+
+--Example:
+--cutomers.customer_id >> PRIMARY KEY
+--orders.customer_id >> FOREIGN KEY
+
+--Relationship:
+--orders.customer_id = customer.customer_id
+
+--Tables don't talk directly.
+--They talk using common columns (keys).
